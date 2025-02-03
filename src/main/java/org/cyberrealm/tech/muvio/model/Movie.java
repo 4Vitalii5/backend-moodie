3f1d16e1b0f58227aa8f4c1d2242c41b4a2aee0c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-03 18:33:55 +0200

    Added entity Movie

--- src/main/java/org/cyberrealm/tech/muvio/model/Movie.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Movie.java
@@ -1,18 +1,12 @@
 package org.cyberrealm.tech.muvio.model;
 
-import java.util.HashSet;
-import java.util.Set;
-import lombok.Data;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
-@Document(collection = "movies")
+@Document("movie")
 public class Movie {
     @Id
     private String id;
-    private String name;
-    private Set<String> genres = new HashSet<>();
-    private double rating;
-    private String trailer;
+
+
 }
