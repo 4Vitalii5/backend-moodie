af4ecaad8ea5d463ac8e0166814ccd345e434d45
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-03 18:34:26 +0200

    Added MovieController

--- src/main/java/org/cyberrealm/tech/muvio/controller/MovieController.java
+++ src/main/java/org/cyberrealm/tech/muvio/controller/MovieController.java
@@ -2,45 +2,21 @@ package org.cyberrealm.tech.muvio.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.cyberrealm.tech.muvio.model.Movie;
-import org.cyberrealm.tech.muvio.service.MovieService;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.cyberrealm.tech.muvio.dto.MovieDto;
+import org.cyberrealm.tech.muvio.repository.movies.MovieRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/movies")
-@RequiredArgsConstructor
+@RequiredArgsConstructor()
+@RequestMapping("movies")
 public class MovieController {
-    private final MovieService movieService;
+    private final MovieRepository movieRepository;
 
     @GetMapping
-    public List<Movie> getAllMovies() {
-        return movieService.getAllMovies();
-    }
-
-    @GetMapping("/{id}")
-    public Movie getMovieById(@PathVariable String id) {
-        return movieService.getMovieById(id);
-    }
-
-    @PostMapping
-    public Movie saveMovie(@RequestBody Movie movie) {
-        return movieService.saveMovie(movie);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteMovieById(@PathVariable String id) {
-        movieService.deleteMovieById(id);
-    }
-
-    @PutMapping("/{id}")
-    public Movie updateMovie(@PathVariable String id, @RequestBody Movie movie) {
-        return movieService.updateMovie(id, movie);
+    public List<MovieDto> getAll(Pageable pageable) {
+        return List.of();
     }
 }
