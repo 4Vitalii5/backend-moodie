81574c812feb15cf4a974769cff8e3b5a086e565
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-05 10:44:59 +0200

    Added CreateMovieRequestDto

--- src/main/java/org/cyberrealm/tech/muvio/dto/CreateMovieRequestDto.java
+++ src/main/java/org/cyberrealm/tech/muvio/dto/CreateMovieRequestDto.java
@@ -1,4 +1,16 @@
 package org.cyberrealm.tech.muvio.dto;
 
-public record CreateMovieRequestDto() {
+import java.time.LocalDate;
+import java.util.Set;
+import org.cyberrealm.tech.muvio.model.Genre;
+
+public record CreateMovieRequestDto(
+        String title,
+        Set<Genre> genres,
+        String overview,
+        Double voteAverage,
+        String posterPath,
+        LocalDate releaseDate,
+        String videoUrl
+) {
 }
