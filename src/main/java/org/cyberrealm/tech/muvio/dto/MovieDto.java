2ef42e9eee6d22bf47e27b4d4495b442b6eaf756
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-05 10:49:13 +0200

    Added MovieDto

--- src/main/java/org/cyberrealm/tech/muvio/dto/MovieDto.java
+++ src/main/java/org/cyberrealm/tech/muvio/dto/MovieDto.java
@@ -1,4 +1,17 @@
 package org.cyberrealm.tech.muvio.dto;
 
-public record MovieDto() {
+import java.time.LocalDate;
+import java.util.Set;
+import org.cyberrealm.tech.muvio.model.Genre;
+
+public record MovieDto(
+        String id,
+        String title,
+        Set<Genre> genres,
+        String overview,
+        Double voteAverage,
+        String posterPath,
+        LocalDate releaseDate,
+        String videoUrl
+) {
 }
