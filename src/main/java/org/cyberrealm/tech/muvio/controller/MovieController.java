25f439a6c874c385ecc090b7af6e268411086c64
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-05 10:48:30 +0200

    Added MovieController

--- src/main/java/org/cyberrealm/tech/muvio/controller/MovieController.java
+++ src/main/java/org/cyberrealm/tech/muvio/controller/MovieController.java
@@ -1,11 +1,20 @@
 package org.cyberrealm.tech.muvio.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.cyberrealm.tech.muvio.dto.CreateMovieRequestDto;
 import org.cyberrealm.tech.muvio.dto.MovieDto;
-import org.cyberrealm.tech.muvio.repository.movies.MovieRepository;
+import org.cyberrealm.tech.muvio.dto.MovieSearchParameters;
+import org.cyberrealm.tech.muvio.dto.UpdateMovieRequestDto;
+import org.cyberrealm.tech.muvio.service.MovieService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +22,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor()
 @RequestMapping("movies")
 public class MovieController {
-    private final MovieRepository movieRepository;
+    private final MovieService movieService;
 
     @GetMapping
     public List<MovieDto> getAll(Pageable pageable) {
-        return List.of();
+        return movieService.findAll(pageable);
+    }
+
+    @PostMapping
+    public MovieDto createMovie(CreateMovieRequestDto requestDto) {
+        return movieService.save(requestDto);
+    }
+
+    @GetMapping("/{id}")
+    public MovieDto getMovieById(@PathVariable String id) {
+        return movieService.findById(id);
+    }
+
+    @PutMapping("{id}")
+    public MovieDto updateMovie(@PathVariable String id,
+                                @RequestBody @Valid UpdateMovieRequestDto requestDto) {
+        return movieService.updateMovieById(id, requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable String id) {
+        movieService.deleteById(id);
+    }
+
+    @GetMapping("/search")
+    public List<MovieDto> searchMovies(@Valid MovieSearchParameters searchParameters,
+                                       Pageable pageable) {
+        return movieService.search(searchParameters, pageable);
     }
 }
