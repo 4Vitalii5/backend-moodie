18c901d3dd824556b6cec33316aa3311ee7f666d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-18 22:25:50 +0200

    Added TmdbProcessingException

---
+++ src/main/java/org/cyberrealm/tech/muvio/exception/TmdbProcessingException.java
@@ -0,0 +1,7 @@
+package org.cyberrealm.tech.muvio.exception;
+
+public class TmdbProcessingException extends RuntimeException {
+    public TmdbProcessingException(String message) {
+        super(message);
+    }
+}
