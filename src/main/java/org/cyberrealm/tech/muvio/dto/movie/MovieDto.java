6b75387ef2b3359f273a23590e3ecddb3c2b8eab
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-18 22:18:12 +0200

    Changed line separator from CRLF to LF

--- src/main/java/org/cyberrealm/tech/muvio/dto/movie/MovieDto.java
+++ src/main/java/org/cyberrealm/tech/muvio/dto/movie/MovieDto.java
@@ -2,16 +2,24 @@ package org.cyberrealm.tech.muvio.dto.movie;
 
 import java.time.LocalDate;
 import java.util.Set;
-import org.cyberrealm.tech.muvio.model.Genre;
+import org.cyberrealm.tech.muvio.model.Actor;
+import org.cyberrealm.tech.muvio.model.Director;
+import org.cyberrealm.tech.muvio.model.GenreEntity;
+import org.cyberrealm.tech.muvio.model.Photo;
+import org.cyberrealm.tech.muvio.model.Review;
 
 public record MovieDto(
         String id,
         String title,
-        Set<Genre> genres,
+        Set<GenreEntity> genres,
+        Set<Actor> actors,
+        Set<Photo> photos,
+        Set<Review> reviews,
+        Director director,
+        String trailerPath,
         String overview,
         Double voteAverage,
         String posterPath,
-        LocalDate releaseDate,
-        String videoUrl
+        LocalDate releaseDate
 ) {
 }
