046b88dde2d11aa68dbf497902d53dead31a22ca
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-18 22:22:00 +0200

    Added config class for tmdb api

---
+++ src/main/java/org/cyberrealm/tech/muvio/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.cyberrealm.tech.muvio.config;
+
+import info.movito.themoviedbapi.TmdbApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    @Value("${tmdb.api.key}")
+    private String apiKey;
+
+    @Bean
+    public TmdbApi tmdbApi() {
+        return new TmdbApi(apiKey);
+    }
+}
