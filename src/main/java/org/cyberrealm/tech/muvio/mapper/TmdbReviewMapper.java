544f973e7b9f43a79e37e42351d7cbe68ba7095d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-18 22:24:31 +0200

    Added TmdbReviewMapper

---
+++ src/main/java/org/cyberrealm/tech/muvio/mapper/TmdbReviewMapper.java
@@ -0,0 +1,13 @@
+package org.cyberrealm.tech.muvio.mapper;
+
+import org.cyberrealm.tech.muvio.config.MapperConfig;
+import org.cyberrealm.tech.muvio.model.Review;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface TmdbReviewMapper {
+    @Mapping(source = "authorDetails.avatarPath", target = "avatarPath")
+    @Mapping(source = "authorDetails.rating", target = "rating")
+    Review toReview(info.movito.themoviedbapi.model.core.Review review);
+}
