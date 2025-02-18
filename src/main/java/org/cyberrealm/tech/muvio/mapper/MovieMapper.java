7293b8a758f3530d91f3444e5beaae6adeef1b65
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-18 22:21:00 +0200

    Updated Movie to MovieEntity

--- src/main/java/org/cyberrealm/tech/muvio/mapper/MovieMapper.java
+++ src/main/java/org/cyberrealm/tech/muvio/mapper/MovieMapper.java
@@ -4,16 +4,16 @@ import org.cyberrealm.tech.muvio.config.MapperConfig;
 import org.cyberrealm.tech.muvio.dto.movie.CreateMovieRequestDto;
 import org.cyberrealm.tech.muvio.dto.movie.MovieDto;
 import org.cyberrealm.tech.muvio.dto.movie.UpdateMovieRequestDto;
-import org.cyberrealm.tech.muvio.model.Movie;
+import org.cyberrealm.tech.muvio.model.MovieEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface MovieMapper {
-    MovieDto toDto(Movie movie);
+    MovieDto toDto(MovieEntity movie);
 
-    Movie toMovie(CreateMovieRequestDto requestDto);
+    MovieEntity toMovie(CreateMovieRequestDto requestDto);
 
     void updateMovieFromDto(UpdateMovieRequestDto requestDto,
-                            @MappingTarget Movie movie);
+                            @MappingTarget MovieEntity movie);
 }
