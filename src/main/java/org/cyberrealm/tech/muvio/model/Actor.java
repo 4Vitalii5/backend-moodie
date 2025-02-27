52b69761911f49692892bd7e818addb3f9f182ee
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-27 16:08:03 +0200

    Changed id annotation to MongoId

--- src/main/java/org/cyberrealm/tech/muvio/model/Actor.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Actor.java
@@ -1,15 +1,15 @@
 package org.cyberrealm.tech.muvio.model;
 
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
 @Document("actors")
 public class Actor {
-    @Id
+    @MongoId
     private String id;
     private String name;
     private String profilePath;
