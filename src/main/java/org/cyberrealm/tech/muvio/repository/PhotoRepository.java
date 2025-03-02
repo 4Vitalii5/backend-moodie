3e4ac0d77bcd3c44c6e2e05ef9f2c773be7cdae9
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:23:04 +0200

    Removed PhotoRepository

--- src/main/java/org/cyberrealm/tech/muvio/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cyberrealm.tech.muvio.repository;
-
-import org.cyberrealm.tech.muvio.model.Photo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhotoRepository extends MongoRepository<Photo, String> {
-}
