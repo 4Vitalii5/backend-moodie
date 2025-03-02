846df9ba123dc1cba5fecf7923b99e1b3e584df7
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:06:20 +0200

    Made Photo as an inner class

--- src/main/java/org/cyberrealm/tech/muvio/model/Photo.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Photo.java
@@ -2,14 +2,9 @@ package org.cyberrealm.tech.muvio.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
-@Document("photos")
 public class Photo {
-    @MongoId
-    private String id;
     private String filePath;
 }
