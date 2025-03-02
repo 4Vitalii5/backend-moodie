2bdc0d072e815b4ed6ad35f43fee7d6389a28a6d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:19:28 +0200

    Removed ActorService

--- src/main/java/org/cyberrealm/tech/muvio/service/ActorService.java
+++
@@ -1,8 +0,0 @@
-package org.cyberrealm.tech.muvio.service;
-
-import java.util.Set;
-import org.cyberrealm.tech.muvio.model.Actor;
-
-public interface ActorService {
-    Set<Actor> getActors(int movieId);
-}
