40d4bfde34c00c03a6f76f7dd766952662202131
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:12:53 +0200

    Changed Set<GenreEntity> to Set<Integer>

--- src/main/java/org/cyberrealm/tech/muvio/dto/movie/UpdateMovieRequestDto.java
+++ src/main/java/org/cyberrealm/tech/muvio/dto/movie/UpdateMovieRequestDto.java
@@ -2,11 +2,10 @@ package org.cyberrealm.tech.muvio.dto.movie;
 
 import java.time.LocalDate;
 import java.util.Set;
-import org.cyberrealm.tech.muvio.model.GenreEntity;
 
 public record UpdateMovieRequestDto(
         String title,
-        Set<GenreEntity> genres,
+        Set<Integer> genresIds,
         String overview,
         Double voteAverage,
         String posterPath,
