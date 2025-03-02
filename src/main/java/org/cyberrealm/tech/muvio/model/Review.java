550c9956821ba41dc5a15adea6c876f06f867e7d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:06:52 +0200

    Made Review as an inner class

--- src/main/java/org/cyberrealm/tech/muvio/model/Review.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Review.java
@@ -2,14 +2,10 @@ package org.cyberrealm.tech.muvio.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
-@Document("reviews")
 public class Review {
-    @MongoId
     private String id;
     private String author;
     private String avatarPath;
