4010d5928916dd5d90af8e511e8ef7cc48244990
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:19:12 +0200

    Removed ActorRepository

--- src/main/java/org/cyberrealm/tech/muvio/repository/ActorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cyberrealm.tech.muvio.repository;
-
-import org.cyberrealm.tech.muvio.model.Actor;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ActorRepository extends MongoRepository<Actor, String> {
-}
