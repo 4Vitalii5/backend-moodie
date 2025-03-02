51eb13df1a19978ffc0dcb6de430ecdf1dc2ac7a
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:23:22 +0200

    Removed ReviewRepository

--- src/main/java/org/cyberrealm/tech/muvio/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cyberrealm.tech.muvio.repository;
-
-import org.cyberrealm.tech.muvio.model.Review;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReviewRepository extends MongoRepository<Review, String> {
-}
