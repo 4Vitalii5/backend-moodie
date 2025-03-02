cd26350f2cdda743647c43817b9e7bdf5946c169
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:04:26 +0200

    Made Director as an inner class

--- src/main/java/org/cyberrealm/tech/muvio/model/Director.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Director.java
@@ -2,14 +2,10 @@ package org.cyberrealm.tech.muvio.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
-@Document("producers")
 public class Director {
-    @MongoId
     private String id;
     private String originalName;
 }
