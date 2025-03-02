aa2bcbc4de384c5638f8b5d17466333635da9887
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:15:29 +0200

    Added constants for image and trailer urls. Fulfill path of photos and videos

--- src/main/java/org/cyberrealm/tech/muvio/service/impl/TmdbClientService.java
+++ src/main/java/org/cyberrealm/tech/muvio/service/impl/TmdbClientService.java
@@ -1,8 +1,9 @@
 package org.cyberrealm.tech.muvio.service.impl;
 
 import info.movito.themoviedbapi.TmdbApi;
-import info.movito.themoviedbapi.model.core.Genre;
+import info.movito.themoviedbapi.model.core.IdElement;
 import info.movito.themoviedbapi.model.core.Movie;
+import info.movito.themoviedbapi.model.core.Review;
 import info.movito.themoviedbapi.model.core.image.Artwork;
 import info.movito.themoviedbapi.model.core.video.Video;
 import info.movito.themoviedbapi.model.movies.Cast;
@@ -18,21 +19,27 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TmdbClientService {
+    private static final String YOUTUBE_PATH = "https://www.youtube.com/watch?v=";
+    private static final String IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
     private static final String EN = "en";
     private static final String UKRAINE = "UA";
     private static final int PAGE = 1;
     private static final int MAX_NUMBER_OF_PAGES = 5;
+    private static final int MAX_NUMBER_OF_RECORDS = 9;
     private static final String TRAILER = "Trailer";
     private static final String YOU_TUBE = "YouTube";
     private static final String DIRECTOR = "Director";
 
     private final TmdbApi tmdbApi;
 
-    public List<Genre> fetchGenres() {
+    public List<Integer> fetchGenres() {
         try {
-            return tmdbApi.getGenre().getMovieList(EN);
+            return tmdbApi.getGenre().getMovieList(EN).stream()
+                    .map(IdElement::getId)
+                    .toList();
         } catch (TmdbException e) {
-            throw new TmdbProcessingException("Can't load genres from TMDB: " + e.getMessage());
+            throw new TmdbProcessingException("Can't load genres from TMDB: "
+                    + e.getMessage());
         }
     }
 
@@ -52,7 +59,9 @@ public class TmdbClientService {
 
     public List<Cast> fetchMovieCast(int movieId) {
         try {
-            return tmdbApi.getMovies().getCredits(movieId, EN).getCast();
+            return tmdbApi.getMovies().getCredits(movieId, EN).getCast().stream()
+                    .peek(cast -> cast.setProfilePath(IMAGE_PATH + cast.getProfilePath()))
+                    .toList();
         } catch (TmdbException e) {
             throw new TmdbProcessingException("Can't load cast from TMDB: " + e.getMessage());
         }
@@ -60,15 +69,24 @@ public class TmdbClientService {
 
     public List<Artwork> fetchMoviePhotos(int movieId) {
         try {
-            return tmdbApi.getMovies().getImages(movieId, EN).getPosters();
+            return tmdbApi.getMovies().getImages(movieId, EN).getBackdrops().stream()
+                    .limit(MAX_NUMBER_OF_RECORDS)
+                    .peek(artwork ->
+                            artwork.setFilePath(IMAGE_PATH + artwork.getFilePath()))
+                    .toList();
         } catch (TmdbException e) {
             throw new TmdbProcessingException("Can't load photos from TMDB: " + e.getMessage());
         }
     }
 
-    public List<info.movito.themoviedbapi.model.core.Review> fetchMovieReviews(int movieId) {
+    public List<Review> fetchMovieReviews(int movieId) {
         try {
-            return tmdbApi.getMovies().getReviews(movieId, EN, PAGE).getResults();
+            return tmdbApi.getMovies().getReviews(movieId, EN, PAGE).getResults().stream()
+                    .limit(50)
+                    .peek(review -> review.getAuthorDetails().setAvatarPath(
+                            IMAGE_PATH + review.getAuthorDetails().getAvatarPath()
+                    ))
+                    .toList();
         } catch (TmdbException e) {
             throw new TmdbProcessingException("Can't load reviews from TMDB: " + e.getMessage());
         }
@@ -78,6 +96,7 @@ public class TmdbClientService {
         try {
             return tmdbApi.getMovies().getCredits(movieId, EN).getCrew().stream()
                     .filter(crew -> DIRECTOR.equalsIgnoreCase(crew.getJob()))
+                    .peek(crew -> crew.setProfilePath(IMAGE_PATH + crew.getProfilePath()))
                     .findFirst();
         } catch (TmdbException e) {
             throw new TmdbProcessingException("Can't load director from TMDB: " + e.getMessage());
@@ -90,6 +109,7 @@ public class TmdbClientService {
                     .filter(video -> TRAILER.equalsIgnoreCase(video.getType())
                             && YOU_TUBE.equalsIgnoreCase(video.getSite()))
                     .map(Video::getKey)
+                    .map(YOUTUBE_PATH::concat)
                     .findFirst()
                     .orElse(null);
         } catch (TmdbException e) {
@@ -108,8 +128,7 @@ public class TmdbClientService {
 
     //    public List<Provider> fetchProviders(int movieId) {
     //        try {
-    //            return tmdbApi.getMovies().getWatchProviders(movieId).getResults().get("UA")
-    //                    .getRentProviders();
+    //            return tmdbApi.getMovies().getWatchProviders(movieId)
     //        } catch (TmdbException e) {
     //            throw new TmdbProcessingException("Can't load movie providers from TMDB: "
     //                    + e.getMessage());
