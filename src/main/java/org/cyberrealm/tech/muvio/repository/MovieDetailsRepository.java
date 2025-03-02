a74b2ce599630208d9c0eb3f71bd91604512a1b4
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:25:12 +0200

    Removed MovieDetailsRepository

--- src/main/java/org/cyberrealm/tech/muvio/repository/MovieDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cyberrealm.tech.muvio.repository;
-
-import org.cyberrealm.tech.muvio.model.MovieDetails;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MovieDetailsRepository extends MongoRepository<MovieDetails, String> {
-}
