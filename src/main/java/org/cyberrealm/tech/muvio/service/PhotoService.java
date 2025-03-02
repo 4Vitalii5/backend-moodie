886ce0d329ec58e5c4ee04face11d588ab0138de
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:24:03 +0200

    Removed PhotoService

--- src/main/java/org/cyberrealm/tech/muvio/service/PhotoService.java
+++
@@ -1,8 +0,0 @@
-package org.cyberrealm.tech.muvio.service;
-
-import java.util.Set;
-import org.cyberrealm.tech.muvio.model.Photo;
-
-public interface PhotoService {
-    Set<Photo> getPhotos(int movieId);
-}
