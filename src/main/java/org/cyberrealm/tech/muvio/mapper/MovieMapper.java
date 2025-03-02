f16f7210b99555910583be04da91973acf108354
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:13:55 +0200

    Added method from tmdb Movie to MovieEntity

--- src/main/java/org/cyberrealm/tech/muvio/mapper/MovieMapper.java
+++ src/main/java/org/cyberrealm/tech/muvio/mapper/MovieMapper.java
@@ -1,11 +1,17 @@
 package org.cyberrealm.tech.muvio.mapper;
 
+import info.movito.themoviedbapi.model.core.Movie;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.cyberrealm.tech.muvio.config.MapperConfig;
 import org.cyberrealm.tech.muvio.dto.movie.CreateMovieRequestDto;
 import org.cyberrealm.tech.muvio.dto.movie.MovieDto;
 import org.cyberrealm.tech.muvio.dto.movie.UpdateMovieRequestDto;
 import org.cyberrealm.tech.muvio.model.MovieEntity;
+import org.cyberrealm.tech.muvio.model.MovieGenre;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
@@ -14,6 +20,23 @@ public interface MovieMapper {
 
     MovieEntity toMovie(CreateMovieRequestDto requestDto);
 
+    @Mapping(target = "id", expression = "java(String.valueOf(tmdbMovie.getId()))")
+    MovieEntity toMovie(Movie tmdbMovie);
+
     void updateMovieFromDto(UpdateMovieRequestDto requestDto,
                             @MappingTarget MovieEntity movie);
+
+    @AfterMapping
+    default Set<MovieGenre> mapToGenre(Set<Integer> genreIds) {
+        return genreIds.stream()
+                .map(MovieGenre::fromId) // Assuming you have a method to convert ID to enum
+                .collect(Collectors.toSet());
+    }
+
+    @AfterMapping
+    default Set<Integer> mapToIds(Set<MovieGenre> genres) {
+        return genres.stream()
+                .map(MovieGenre::getId) // Assuming you have a method getId() in enum
+                .collect(Collectors.toSet());
+    }
 }
