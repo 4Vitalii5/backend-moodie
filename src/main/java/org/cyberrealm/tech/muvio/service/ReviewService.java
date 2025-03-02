e19c3015754c205496598991200bfae7300cc82d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:23:46 +0200

    Removed ReviewService

--- src/main/java/org/cyberrealm/tech/muvio/service/ReviewService.java
+++
@@ -1,8 +0,0 @@
-package org.cyberrealm.tech.muvio.service;
-
-import java.util.Set;
-import org.cyberrealm.tech.muvio.model.Review;
-
-public interface ReviewService {
-    Set<Review> getReviews(int movieId);
-}
