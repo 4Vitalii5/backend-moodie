6cd5a1ba5d672112d40f9bedfebea42edd2b88d9
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:22:24 +0200

    Removed DirectorRepository

--- src/main/java/org/cyberrealm/tech/muvio/repository/DirectorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cyberrealm.tech.muvio.repository;
-
-import org.cyberrealm.tech.muvio.model.Director;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DirectorRepository extends MongoRepository<Director, String> {
-}
