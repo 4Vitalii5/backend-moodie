d2ea5cd2ae90c3ab2e5801fe8c8d8faaa28d33c9
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:24:18 +0200

    Removed GenreService

--- src/main/java/org/cyberrealm/tech/muvio/service/GenreService.java
+++
@@ -1,8 +0,0 @@
-package org.cyberrealm.tech.muvio.service;
-
-import java.util.List;
-import org.cyberrealm.tech.muvio.model.GenreEntity;
-
-public interface GenreService {
-    List<GenreEntity> loadGenresIntoRepository();
-}
