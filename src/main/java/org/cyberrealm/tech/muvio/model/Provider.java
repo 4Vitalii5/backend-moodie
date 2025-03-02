9255a76b279530af77ffdef6641ddf75d9a60122
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:06:35 +0200

    Made Photo as an inner class

--- src/main/java/org/cyberrealm/tech/muvio/model/Provider.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Provider.java
@@ -2,16 +2,11 @@ package org.cyberrealm.tech.muvio.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
-@Document("providers")
 public class Provider {
-    @MongoId
-    private ObjectId id;
+    private String id;
     private Integer displayPriority;
     private String logoPath;
     private String providerName;
