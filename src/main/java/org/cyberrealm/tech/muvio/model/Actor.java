48b492ae431b2ec50a61176c96e567afb69f457a
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-02 14:03:45 +0200

    Made Actor as an inner class

--- src/main/java/org/cyberrealm/tech/muvio/model/Actor.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Actor.java
@@ -2,14 +2,10 @@ package org.cyberrealm.tech.muvio.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
-@Document("actors")
 public class Actor {
-    @MongoId
     private String id;
     private String name;
     private String profilePath;
